c228c61f4e16598d44555ff6fde5d1fe88977c6d
Nina Popovici <adrenocrom_pn@yahoo.com>
2024-05-30 19:34:18 +0100

    trying to fix registration

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -32,17 +32,14 @@ const authMiddleware = (req, res, next) => {
 router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
-    // console.log(req.body);
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    //create user
     try {
       const user = await User.create({ username, password: hashedPassword });
-      res.redirect("/dashboard");
-       res.status(200).json({ message: "User created", user });
+      res.status(201).json({ message: "User Created", user });
     } catch (error) {
       if (error.code === 11000) {
-        res.status(409).json({ message: "Username already exists" });
+        res.status(409).json({ message: "User already in use" });
       }
       res.status(500).json({ message: "Internal server error" });
     }
